5fa41cef4f41f121485bbe61942304ac81a663f2
Paritosh <kaushikpk007@gmail.com>
2026-05-24 15:04:39 -0700

    Created DailyReturns model

--- data/collector/dal/models/DailyReturns.py
+++ data/collector/dal/models/DailyReturns.py
@@ -1,6 +1,20 @@
 from data.collector.dal.models import BaseModel
-
+import datetime
 
 class DailyReturns(BaseModel):
-    def __init__(self, id):
-        self.id
+    def __init__(self, id,
+                 date: datetime,
+                 open: float,
+                 high: float,
+                 low: float,
+                 close: float,
+                 volume: int):
+        super.__init__(id)
+        self.date = date
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
+        self.volume = volume
+
+
