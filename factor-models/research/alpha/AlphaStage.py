95c4d37c37e484d8dab7545635e138812aebbd10
Paritosh <kaushikpk007@gmail.com>
2026-06-29 23:23:04 -0700

    Adding alpha strategy for stage DAG pattern

--- factor-models/research/alpha/AlphaStage.py
+++ factor-models/research/alpha/AlphaStage.py
@@ -1,10 +1,11 @@
+from Alphas import Alphas
 
 class AlphaStage:
     def __init__(self, config: list[dict]):
         self._strategies = []
 
     @property
-    def strategies(self) -> list:
+    def strategies(self) -> list[Alphas]:
         return self._strategies
 
     @strategies.setter
---
+++ factor-models/research/alpha/Alphas.py
@@ -0,0 +1,8 @@
+from abc import abstractmethod, ABC
+
+
+class Alphas(ABC):
+
+    @abstractmethod
+    def execute(self):
+        pass
